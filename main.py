2452dc07100700f2768680fd694108cbc7271b13
rai369770-ship-it <rai369770@gmail.com>
2026-03-30 15:23:08 +0545

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = FastAPI()
 @app.get("/")
 def root():
     return JSONResponse({
-        "hasMessage": False,
-        "title": "v1.3 launch",
-        "message": "Version 1.3 is pack of brand new features on the extension. I am very excited to share the new version. Visit our telegram channel for future updates and features.",
+        "hasMessage": True,
+        "title": "v1.3 successfully launched.",
+        "message": "I have successfully launched a new version v1.3. V1.3 presents AI summery as the major feature of the update. The version also brings enhancements in user experience and plugin stability. Thanks for your continued support and love for AI voice dictation jieshuo plugin.",
         "hasLink": True,
-        "link": "t.me/blindtechvisionary"
+        "link": "https://t.me/blindtechvisionary"
     })
