d102b691f5311c58279632070f01eb54125b5942
rai369770-ship-it <rai369770@gmail.com>
2026-03-30 14:06:26 +0545

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return JSONResponse({
-        "hasMessage": True,
+        "hasMessage": False,
         "title": "v1.3 launch",
         "message": "Version 1.3 is pack of brand new features on the extension. I am very excited to share the new version. Visit our telegram channel for future updates and features.",
         "hasLink": True,
