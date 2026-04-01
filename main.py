fa6b947298da4c448d73c9b5517730b7c7eea891
rai369770-ship-it <rai369770@gmail.com>
2026-04-01 20:39:44 +0545

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return JSONResponse({
-        "hasMessage": True,
+        "hasMessage": False,
         "title": "v1.3 successfully launched.",
         "message": "I have successfully launched a new version v1.3. V1.3 presents AI summery as the major feature of the update. The version also brings enhancements in user experience and plugin stability. Thanks for your continued support and love for AI voice dictation jieshuo plugin.",
         "hasLink": True,
